afc71e8b955898ca85db21e6a13e47c4eea6b1c1
Malcolm Jones <malcolm@adobe.com>
2018-02-12 23:39:34 -0500

    chg: locales

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,10 +5,13 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_locales(host):
+    f = host.file('/etc/default/locale')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+    # assert f.contains("LANG=en_US.UTF-8")
+    # assert f.contains("LC_ALL=en_US.UTF-8")
+
+
+
